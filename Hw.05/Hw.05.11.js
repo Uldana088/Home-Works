d689a9a6f84d039c5e7add793cf813e3e2be786a
Abdiomar Uldana <Abdiumar@list.ru>
2024-11-05 23:08:01 +0500

    Home work 05.11

---
+++ Hw.05/Hw.05.11.js
@@ -0,0 +1,21 @@
+// task 1
+
+var age = prompt("Enter your age");
+
+if (age >= 18) {
+    alert("Сіз ересексіз!");
+} else { 
+    alert("сіз кәмелетке толмағансыз!");
+}
+
+
+// task 2
+
+
+var temp = prompt("Enter temperature");
+
+if ((temp > 25)&& (temp < 25) ) {
+    alert("Қалың куртка, жемпір кисе дұрыс")
+} else{
+    alert("Жеңіл шорты, футболка кисе жеткілікті")
+}
